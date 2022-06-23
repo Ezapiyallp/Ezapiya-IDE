fec82cf5730c7753f893d93029b915f5dae65af2
vinit <vinit.ramteke@gmail.com>
2022-06-23 21:16:48 +0530

    Add Editor

--- cls_main_from.py
+++ cls_main_from.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets
 from PyQt5.QtCore import QSize
 
 from mainForm import Ui_MainWindow
+from SimplePythonEditor import SimplePythonEditor
 import icon_qrc
 class cls_main_from(QtWidgets.QMainWindow):
     def __init__(self):
@@ -9,3 +10,17 @@ class cls_main_from(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.dockWidget_message.setFloating(False)
+        self.ui.tabWidget.setTabsClosable(True)
+        self.ui.tabWidget.removeTab(0)
+        self.ui.tabWidget.removeTab(0)
+        self.ui.tabWidget.tabCloseRequested.connect(self.closeActiveTab)
+        self.editor = SimplePythonEditor()
+        self.ui.tabWidget.addTab(self.editor, 'New File')
+        self.ui.actionNew.triggered.connect(self.newfile_action)
+    def closeActiveTab(self):
+        self.ui.tabWidget.removeTab(self.ui.tabWidget.currentIndex())
+        if self.ui.tabWidget.count() == 0:
+            self.ui.tabWidget.addTab(self.editor, 'New File')
+
+    def newfile_action(self):
+        self.ui.tabWidget.addTab(self.editor, 'New File')
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from cls_main_from import cls_main_from
 import sys
 app = QtWidgets.QApplication([])
 main_f = cls_main_from()
+main_f.setWindowState(QtCore.Qt.WindowMaximized)
 main_f.show()
 
 sys.exit(app.exec_())
