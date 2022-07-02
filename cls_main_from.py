cd11b0e9885d126a09d45665a00e0e39b0c2fe6a
Ezapiyallp <89907969+Ezapiyallp@users.noreply.github.com>
2022-07-02 22:05:08 +0530

    All mune connection done

--- cls_main_from.py
+++ cls_main_from.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import QFileDialog
-
+import sys
 from mainForm import Ui_MainWindow
 from SimplePythonEditor import SimplePythonEditor
 import icon_qrc
@@ -19,10 +19,14 @@ class cls_main_from(QtWidgets.QMainWindow):
         self.ui.tabWidget.tabCloseRequested.connect(self.closeTab)
         self.editor = SimplePythonEditor()
         self.ui.tabWidget.addTab(self.editor, 'New File 1')
+            #### File Menu all action
         self.ui.actionNew.triggered.connect(self.newfile_action)
-        self.ui.actionClose_File.triggered.connect(self.closeTab)
         self.ui.actionOpen.triggered.connect(self.openfile_action)
         self.ui.actionSave_File.triggered.connect(self.savefile_actoin)
+        self.ui.actionSave_As.triggered.connect(self.save_as_action)
+        self.ui.actionClose_File.triggered.connect(self.closeTab)
+        self.ui.actionExit.triggered.connect(self.exit_action)
+
             ##### Edit mune All Action #####
         self.ui.actionCut.triggered.connect(self.cut_action)
         self.ui.actionCopy.triggered.connect(self.copy_action)
@@ -33,6 +37,23 @@ class cls_main_from(QtWidgets.QMainWindow):
         self.ui.actionFind.triggered.connect(self.find_actio)
         self.ui.actionReplace.triggered.connect(self.replace_action)
         self.ui.actionGogo.triggered.connect(self.goto_action)
+                ##### View Mune All Action
+        self.ui.actionMessage_Window.triggered.connect(self.message_window_action)
+        self.ui.actiontool_Window.triggered.connect(self.tool_box_action)
+        self.ui.actionProject_Windows.triggered.connect(self.project_window_action)
+        self.ui.actionProparty_Window.triggered.connect(self.property_window_action)
+                    ##### Run Mune All Action
+        self.ui.actionCompile.triggered.connect(self.compile_action)
+        self.ui.actionRun.triggered.connect(self.run_action)
+        self.ui.actionCompile_And_Run.triggered.connect(self.compile_and_run_action)
+                    ###### Debug Mune All Action
+        self.ui.actionStart_Debug.triggered.connect(self.start_debug_action)
+        self.ui.actionExecute_Next_Line.triggered.connect(self.execute_next_line_action)
+        self.ui.actionExecute_Next_Function.triggered.connect(self.execute_next_funtion_action)
+        self.ui.actionGoto_Next_Breakpoint.triggered.connect(self.goto_next_breackpoint_action)
+        self.ui.actionEnd_Debug.triggered.connect(self.end_debug_action)
+        self.ui.actionBreakpoint.triggered.connect(self.breackpoint_action)
+        self.ui.actionWatch.triggered.connect(self.watch_action)
 
     def closeTab (self, currentIndex):
         #currentqwidget = self.ui.tabWidget(currentIndex)
@@ -93,6 +114,11 @@ class cls_main_from(QtWidgets.QMainWindow):
         tfileName = tfileName.split('/')
         fileName = tfileName[len(tfileName) - 1]
         self.changeTitalofActiveTab(fileName)
+    def save_as_action(self):
+        pass
+    def exit_action(self):
+        sys.exit()
+
 
         ###Edit Mune All Action
     def cut_action(self):
@@ -122,3 +148,44 @@ class cls_main_from(QtWidgets.QMainWindow):
         print("replace")
     def goto_action(self):
         print("goto")
+
+        ##### View Mune All Action
+    def message_window_action(self):
+        pass
+    def tool_box_action(self):
+        pass
+    def project_window_action(self):
+        pass
+    def property_window_action(self):
+        pass
+
+        ##### Run Mune All Action
+    def compile_action(self):
+        pass
+        #print("compile")
+    def run_action(self):
+        pass
+        #print("run")
+    def compile_and_run_action(self):
+        pass
+        #print("Run and Compile")
+
+
+
+        ###### Debug Mune All Action
+
+
+    def start_debug_action(self):
+        print("start")
+    def execute_next_line_action(self):
+        print("line")
+    def execute_next_funtion_action(self):
+        print("funtion")
+    def goto_next_breackpoint_action(self):
+        print("goto")
+    def end_debug_action(self):
+        print("end")
+    def breackpoint_action(self):
+        print("breack")
+    def watch_action(self):
+        print("watch")
