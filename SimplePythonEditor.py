fd1bc07c65b8941f2974a4f8f549bb64626e6e87
vinit <vinit.ramteke@gmail.com>
2022-07-04 21:03:01 +0530

    add Colorcode

--- SimplePythonEditor.py
+++ SimplePythonEditor.py
@@ -1,11 +1,6 @@
-
-import os
-import sys
-import PyQt5
 from PyQt5 import Qsci
-from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from PyQt5.Qsci import QsciScintilla, QsciLexerPython,QsciLexerCPP
+from PyQt5.Qsci import QsciScintilla, QsciLexerPython,QsciLexerCPP,QsciLexerJava
 from PyQt5.QtWidgets import QApplication, QShortcut, QFileDialog
 import keyword
 class SimplePythonEditor(QsciScintilla):
@@ -91,6 +86,12 @@ class SimplePythonEditor(QsciScintilla):
         self.setText(f.read())
         f.close()
         self.saveStatus = "Yes"
+
+        if self.fileExtention=="c" or self.fileExtention=="cpp" :
+            self.setCpplexer()
+        if self.fileExtention=="py":
+            self.setPythonlexer()
+
         return self.fullFileName
 
     def getSaveStatus(self):
@@ -109,6 +110,12 @@ class SimplePythonEditor(QsciScintilla):
         f.close()
         self.saveStatus = "Yes"
 
+        if self.fileExtention == "c" or self.fileExtention=="cpp" :
+            self.setCpplexer()
+        if self.fileExtention == "py":
+            self.setPythonlexer()
+
+
     def getFullFileName(self):
         return self.fullFileName
 
@@ -116,3 +123,164 @@ class SimplePythonEditor(QsciScintilla):
         pass
     def setFullFileNmae(self, Fname):
         self.fullFileName = Fname
+
+    def setCpplexer(self):
+        font = QFont()
+        font.setFamily('Courier')
+        font.setFixedPitch(True)
+        font.setPointSize(16)
+        lexer = QsciLexerCPP(self, False)
+        lexer.setDefaultFont(font)
+        lexer.setDefaultPaper(QColor("#3c3c3c"))
+        lexer.setDefaultColor(QColor("#f9f9f9"))
+        lexer.setColor(QColor("#A69C9C"), 0)#  Default = 0
+        lexer.setColor(QColor("#A95151"), 1)# Comment = 1
+        lexer.setColor(QColor("#6C51A9"), 2)# CommentLine = 2
+        lexer.setColor(QColor("#BDA5F4"), 3)# CommentDoc = 3
+        lexer.setColor(QColor("#46939C"), 4)#  Number = 4
+        lexer.setColor(QColor("#C5BA40"), 5)# Keyword = 5
+        lexer.setColor(QColor("#296C39"), 6)#  DoubleQuotedString = 6
+        lexer.setColor(QColor("#96A82F"), 7)#  SingleQuotedString = 7
+        lexer.setColor(QColor("#A87F2F"), 8)#  UUID = 8
+        lexer.setColor(QColor("#FDD68A"), 9)#  PreProcessor = 9
+        lexer.setColor(QColor("#E1898F"), 10)#  Operator = 10
+        lexer.setColor(QColor("#E1898F"), 11)#  Identifier = 1
+        lexer.setColor(QColor("#E1898F"), 12)# UnclosedString = 12
+        lexer.setColor(QColor("#E1898F"), 13)# VerbatimString = 13
+        lexer.setColor(QColor("#E1898F"), 14)# Regex = 14
+        lexer.setColor(QColor("#E1898F"), 15)# CommentLineDoc = 15
+        lexer.setColor(QColor("#E1898F"), 16)# KeywordSet2 = 16
+        lexer.setColor(QColor("#E1898F"), 17)#  CommentDocKeyword = 17
+        lexer.setColor(QColor("#E1898F"), 18)# CommentDocKeywordError = 18
+        lexer.setColor(QColor("#E1898F"), 19)# GlobalClass = 19
+        lexer.setColor(QColor("#E1898F"), 20)# RawString = 20
+        lexer.setColor(QColor("#E1898F"), 21)# TripleQuotedVerbatimString = 21
+        lexer.setColor(QColor("#E1898F"), 22)# HashQuotedString = 22
+        lexer.setColor(QColor("#E1898F"), 23)#  PreProcessorComment = 23,
+        lexer.setColor(QColor("#E1898F"), 24)#   PreProcessorCommentLineDoc = 2
+        lexer.setColor(QColor("#E1898F"), 25)# UserLiteral = 25
+        lexer.setColor(QColor("#E1898F"), 26)# TaskMarker = 26
+        lexer.setColor(QColor("#E1898F"), 27)# EscapeSequence = 27
+        self.setCaretForegroundColor(QColor("#FFFFFF"))
+        my_list = [".", " ", "-", ":", ";"]
+        self.autoCompletionFillups()
+        self.setAutoCompletionWordSeparators(my_list)
+        self.setLexer(lexer)
+        api = Qsci.QsciAPIs(lexer)
+        for key in keyword.kwlist + dir(__builtins__):
+            api.add(key)
+        api.add("aLongString")
+        api.add("aLongerString")
+        api.add("aDifferentString")
+        api.add("sOmethingElse")
+        api.prepare()
+        self.setAutoCompletionCaseSensitivity(False)
+        self.setAutoCompletionReplaceWord(False)
+        self.setAutoCompletionThreshold(1)
+        self.setAutoCompletionSource(Qsci.QsciScintilla.AcsAll)
+        self.SendScintilla(QsciScintilla.SCI_SETHSCROLLBAR, 0)
+        shortcut_ctrl_space = QShortcut(QKeySequence("Ctrl+Space"), self);
+        # self.connect(shortcut_ctrl_space, SIGNAL(activated()), self,SLOT(autoCompleteFromAll()));
+
+    def setPythonlexer(self):
+        font = QFont()
+        font.setFamily('Courier')
+        font.setFixedPitch(True)
+        font.setPointSize(16)
+        lexer = QsciLexerPython(self, False)
+        lexer.setDefaultFont(font)
+        lexer.setDefaultPaper(QColor("#3c3c3c"))
+        lexer.setDefaultColor(QColor("#f9f9f9"))
+        lexer.setColor(QColor("#A69C9C"), 0)# Default = 0
+        lexer.setColor(QColor("#A95151"), 1)# Comment = 1
+        lexer.setColor(QColor("#6C51A9"), 2)#  Number = 2
+        lexer.setColor(QColor("#BDA5F4"), 3)#  DoubleQuotedString = 3
+        lexer.setColor(QColor("#46939C"), 4)# SingleQuotedString = 4
+        lexer.setColor(QColor("#C5BA40"), 5)#    Keyword = 5
+        lexer.setColor(QColor("#296C39"), 6)# TripleSingleQuotedString = 6
+        lexer.setColor(QColor("#96A82F"), 7)# TripleDoubleQuotedString = 7
+        lexer.setColor(QColor("#A87F2F"), 8)#  ClassName = 8
+        lexer.setColor(QColor("#FDD68A"), 9)#  FunctionMethodName = 9
+        lexer.setColor(QColor("#E1898F"), 10)#  Operator = 10
+        lexer.setColor(QColor("#E1898F"), 11)#  Identifier = 11
+        lexer.setColor(QColor("#E1898F"), 12)# CommentBlock = 12
+        lexer.setColor(QColor("#E1898F"), 13)#  UnclosedString = 13
+        lexer.setColor(QColor("#E1898F"), 14)#  HighlightedIdentifier = 14
+        lexer.setColor(QColor("#E1898F"), 15)# Decorator = 15
+
+        self.setCaretForegroundColor(QColor("#FFFFFF"))
+        my_list = ["."]
+        self.setAutoCompletionWordSeparators(my_list)
+        self.setLexer(lexer)
+        api = Qsci.QsciAPIs(lexer)
+        for key in keyword.kwlist + dir(__builtins__):
+            api.add(key)
+        api.add("aLongString")
+        api.add("aLongerString")
+        api.add("aDifferentString")
+        api.add("sOmethingElse")
+        api.prepare()
+        self.setAutoCompletionCaseSensitivity(False)
+        self.setAutoCompletionReplaceWord(False)
+        self.setAutoCompletionThreshold(1)
+        self.setAutoCompletionSource(Qsci.QsciScintilla.AcsAll)
+        self.SendScintilla(QsciScintilla.SCI_SETHSCROLLBAR, 0)
+        shortcut_ctrl_space = QShortcut(QKeySequence("Ctrl+Space"), self);
+        # self.connect(shortcut_ctrl_space, SIGNAL(activated()), self,SLOT(autoCompleteFromAll()));
+
+    def setJavalexer(self):
+        font = QFont()
+        font.setFamily('Courier')
+        font.setFixedPitch(True)
+        font.setPointSize(16)
+        lexer = QsciLexerJava(self, False)
+        lexer.setDefaultFont(font)
+        lexer.setDefaultPaper(QColor("#3c3c3c"))
+        lexer.setDefaultColor(QColor("#f9f9f9"))
+        lexer.setColor(QColor("#A69C9C"), 0)  # Default = 0
+        lexer.setColor(QColor("#A95151"), 1)  # Comment = 1
+        lexer.setColor(QColor("#6C51A9"), 2)  # CommentLine = 2
+        lexer.setColor(QColor("#BDA5F4"), 3)  # CommentDoc = 3
+        lexer.setColor(QColor("#46939C"), 4)  # Number = 4
+        lexer.setColor(QColor("#C5BA40"), 5)  # Keyword = 5
+        lexer.setColor(QColor("#296C39"), 6)  # DoubleQuotedString = 6
+        lexer.setColor(QColor("#96A82F"), 7)  # SingleQuotedString = 7
+        lexer.setColor(QColor("#A87F2F"), 8)  # UUID = 8
+        lexer.setColor(QColor("#FDD68A"), 9)  # PreProcessor = 9
+        lexer.setColor(QColor("#E1898F"), 10)  # Operator = 10
+        lexer.setColor(QColor("#E1898F"), 11)  # Identifier = 1
+        lexer.setColor(QColor("#E1898F"), 12)  # UnclosedString = 12
+        lexer.setColor(QColor("#E1898F"), 13)  # VerbatimString = 13
+        lexer.setColor(QColor("#E1898F"), 14)  # Regex = 14
+        lexer.setColor(QColor("#E1898F"), 15)  # CommentLineDoc = 15
+        lexer.setColor(QColor("#E1898F"), 16)  # KeywordSet2 = 16
+        lexer.setColor(QColor("#E1898F"), 17)  # CommentDocKeyword = 17
+        lexer.setColor(QColor("#E1898F"), 18)  # CommentDocKeywordError = 18
+        lexer.setColor(QColor("#E1898F"), 19)  # GlobalClass = 19
+        lexer.setColor(QColor("#E1898F"), 20)  # RawString = 20
+        lexer.setColor(QColor("#E1898F"), 21)  # TripleQuotedVerbatimString = 21
+        lexer.setColor(QColor("#E1898F"), 22)  # HashQuotedString = 22
+        lexer.setColor(QColor("#E1898F"), 23)  # PreProcessorComment = 23,
+        lexer.setColor(QColor("#E1898F"), 24)  # PreProcessorCommentLineDoc = 2
+        lexer.setColor(QColor("#E1898F"), 25)  # UserLiteral = 25
+        lexer.setColor(QColor("#E1898F"), 26)  # TaskMarker = 26
+        lexer.setColor(QColor("#E1898F"), 27)  # EscapeSequence = 27
+        self.setCaretForegroundColor(QColor("#FFFFFF"))
+        my_list = ["."]
+        self.setAutoCompletionWordSeparators(my_list)
+        self.setLexer(lexer)
+        api = Qsci.QsciAPIs(lexer)
+        for key in keyword.kwlist + dir(__builtins__):
+            api.add(key)
+        api.add("aLongString")
+        api.add("aLongerString")
+        api.add("aDifferentString")
+        api.add("sOmethingElse")
+        api.prepare()
+        self.setAutoCompletionCaseSensitivity(False)
+        self.setAutoCompletionReplaceWord(False)
+        self.setAutoCompletionThreshold(1)
+        self.setAutoCompletionSource(Qsci.QsciScintilla.AcsAll)
+        self.SendScintilla(QsciScintilla.SCI_SETHSCROLLBAR, 0)
+        shortcut_ctrl_space = QShortcut(QKeySequence("Ctrl+Space"), self);
+        # self.connect(shortcut_ctrl_space, SIGNAL(activated()), self,SLOT(autoCompleteFromAll()));
